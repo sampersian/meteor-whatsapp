2401f8b23c750b317ae346454a90fbae80827d13
Samuel Persian <samuel.persian@gmail.com>
2016-10-31 17:00:09 -0600

    initial commit -> tutorial parts 0-3 complete and working

---
+++ README.md
@@ -0,0 +1,2 @@
+FROM THE TUTORIAL AT:
+https://www.angular-meteor.com/tutorials/whatsapp/meteor/bootstrapping
---
+++ client/scripts/lib/app.js
@@ -0,0 +1,45 @@
+// Libs
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-moment';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
+import { Meteor } from 'meteor/meteor';
+
+// Modules
+import ChatsCtrl from '../controllers/chats.controller';
+import ChatCtrl from '../controllers/chat.controller';
+import InputDirective from '../directives/input.directive';
+import CalendarFilter from '../filters/calendar.filter'
+import RoutesConfig from '../routes';
+
+const App = 'Whatsapp';
+
+// App
+Angular.module(App, [
+  'angular-meteor',
+  'angularMoment',
+  'ionic'
+]);
+
+new Loader(App)
+  .load(ChatsCtrl)
+  .load(ChatCtrl)
+  .load(InputDirective)
+  .load(CalendarFilter)
+  .load(RoutesConfig);
+
+// Startup
+if (Meteor.isCordova) {
+  Angular.element(document).on('deviceready', onReady);
+}
+else {
+  Angular.element(document).ready(onReady);
+}
+
+function onReady() {
+  Angular.bootstrap(document, [App]);
+}
