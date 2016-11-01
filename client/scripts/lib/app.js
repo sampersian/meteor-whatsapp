b9dd57929d27b716b0712c944cb413dc43d94e7d
Samuel Persian <samuel.persian@gmail.com>
2016-10-31 22:09:30 -0600

    thru 4.12

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -13,9 +13,10 @@ import { Meteor } from 'meteor/meteor';
 // Modules
 import ChatsCtrl from '../controllers/chats.controller';
 import ChatCtrl from '../controllers/chat.controller';
+import LoginCtrl from '../controllers/login.controller'
 import InputDirective from '../directives/input.directive';
 import CalendarFilter from '../filters/calendar.filter'
-import RoutesConfig from '../routes';
+import Routes from '../routes';
 
 const App = 'Whatsapp';
 
@@ -30,9 +31,10 @@ Angular.module(App, [
 new Loader(App)
   .load(ChatsCtrl)
   .load(ChatCtrl)
+  .load(LoginCtrl)
   .load(InputDirective)
   .load(CalendarFilter)
-  .load(RoutesConfig);
+  .load(Routes);
 
 // Startup
 if (Meteor.isCordova) {
